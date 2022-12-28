c41b532d8c157f81cc531b9195ca372db0714f9a
ziad <ziad@yotech.ltd>
2022-12-28 13:24:14 +0600

    init commit.dev

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -6,19 +6,38 @@ import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Container(
+        color: Colors.white,
+        width: Get.width,
+        height: Get.height,
+        child: Center(),
       ),
     );
   }
+}
+
+class CustomWave extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    var paint = Paint();
+
+    path.moveTo(0, size.height * 0.84);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.77,
+        size.width * 0.52, size.height * 0.84);
+    // path.quadraticBezierTo(
+    //     size.width * 0.74, size.height * 0.92, size.width, size.height * 0.84);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    paint.color = Colors.red;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
